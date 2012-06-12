88ee7c8f5a5bdc9f169148770f277a46006d3791
Hendrik Bunke <h.bunke@zbw.eu>
2012-06-12 14:25:35 +0200

    another unicode error bug, this time, again in citation_string

--- zbw/ejpdf/browser/cover.py
+++ zbw/ejpdf/browser/cover.py
@@ -156,7 +156,7 @@ class View(BrowserView):
             version = type_view.last_version_info()
             doi_url = "http://dx.doi.org/%s" %type_view.get_doi()
             text += " (%s). " %type_view.pubyear()
-            text += self.context.Title()
+            text += unicode(self.context.Title(), 'utf-8')
             text += ".  Economics: The Open-Access, Open-Assessment E-Journal, "
             text += "Vol. %s, %s" %(type_view.get_volume(), self.context.getId())
             if version and version['number'] > 1:
@@ -166,7 +166,7 @@ class View(BrowserView):
         if pt == "DiscussionPaper":
             type_view = getMultiAdapter((self.context, self.request), name="dp_view")
             url = self.context.absolute_url()
-            text += type_view.cite_as()
+            text += unicode(type_view.cite_as(), 'utf-8')
             text += " %s" %url
 
         return text
