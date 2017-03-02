51058e0aa867902daf52c89f8a66503c8c342533
Hendrik Bunke <h.bunke@zbw.eu>
2017-03-02 16:35:30 +0100

    new logo, journalpaper a4

--- zbw/ejpdf/browser/fo.py
+++ zbw/ejpdf/browser/fo.py
@@ -14,11 +14,12 @@ from zope.component import getMultiAdapter
 from datetime import datetime
 from xml.sax.saxutils import escape
 from operator import itemgetter
+from toolz.itertoolz import first
 
 class View(BrowserView):
 
     template = ViewPageTemplateFile("fo.pt")
-    
+
 
 
     def __call__(self):
@@ -75,19 +76,19 @@ class View(BrowserView):
         if pt == "JournalPaper":
             uri = "http://dx.doi.org/10.5018/economics-ejournal.ja.%s" %self.context.getId()
         return uri
-    
-    
+
+
 
     def clean_abstract(self):
         """
         """
         html = self.context.getAbstract()
-        
+
         #tags we don't need BeautifulSoup for. KISS!
         abstract = html.replace('<br />', '')
-        
+
         soup = BeautifulSoup(abstract, convertEntities=BeautifulSoup.HTML_ENTITIES)
-        
+
         #replacing html <p> with fo <fo:block>
         while True:
             p = soup.find('p')
@@ -97,13 +98,13 @@ class View(BrowserView):
             del p['style']
             del p['align']
             del p['class']
-            
+
             p['text-align'] = 'justify'
             p['space-after'] = '6px'
             p['language'] = 'en'
             p['hyphenate'] = 'false'
-        
-        #replacing sub 
+
+        #replacing sub
         while True:
             sub = soup.find('sub')
             if not sub:
@@ -120,7 +121,7 @@ class View(BrowserView):
             sup.name = 'fo:inline'
             sup['baseline-shift'] = 'sup'
             sup['font-size'] = '80%'
-        
+
         while True:
             em = soup.find('em')
             if not em:
@@ -139,7 +140,7 @@ class View(BrowserView):
         links = soup.findAll('a')
         for l in links:
             l.replaceWithChildren()
-        
+
         return unicode(soup)
 
 
@@ -159,11 +160,11 @@ class View(BrowserView):
         """
         paper_view = getMultiAdapter((self.context, self.request),
                 name="paperView")
-        
+
         date = datetime.now()
 
         text = u""
-        
+
 
         text += unicode(paper_view.authors_as_string(), 'utf-8')
         pt = self.context.portal_type
@@ -171,7 +172,7 @@ class View(BrowserView):
             type_view = getMultiAdapter((self.context, self.request), name="ja_view")
             version = type_view.last_version_info()
             doi_url = "http://dx.doi.org/%s" %type_view.get_doi()
-            
+
             if version:
                 text += " ({}). ".format(version['year'])
             else:
@@ -179,13 +180,13 @@ class View(BrowserView):
 
             title = self.escape_title()
             text += unicode(title, 'utf-8')
-            
+
             signs = ('?', '!', '.')
             if not text.endswith(signs):
                 text += "."
             text += " <fo:inline font-style='italic'>Economics: The Open-Access, Open-Assessment E-Journal</fo:inline>, "
             text += "%s (%s)" %(type_view.get_volume(), self.context.getId())
-            
+
             if self.context.getPages():
                 pages = self.context.getPages()
                 text += u": 1—%s" %pages
@@ -193,11 +194,11 @@ class View(BrowserView):
             if version and version['number'] > 1:
                text += " (Version %s)" %version['number']
             text += ". %s" %doi_url
-        
+
         if pt == "DiscussionPaper":
             type_view = getMultiAdapter((self.context, self.request), name="dp_view")
             url = self.context.absolute_url()
-            
+
             citation = escape(type_view.cite_as())
 
             text += unicode(citation, 'utf-8')
@@ -211,27 +212,27 @@ class View(BrowserView):
         returns dicts with fullname and affiliation
         """
         catalog = getToolByName(self.context, "portal_catalog")
-        
+
         brains = map(lambda author_id: itemgetter(0)(catalog(id=author_id)),
                 self.context.getAuthors())
-        
+
         authors = map(lambda brain: dict(
             author_id = brain.getObject().getId(),
             name = '{} {}'.format(brain.getObject().getFirstname(), brain.getSurname),
             affil = brain.getObject().getOrganisation()
             ), brains)
-        
+
         return authors
 
 
     def authors_as_string(self):
         """
         """
-        
+
         return map(lambda author: self._authors_concat_string(author,
                     self.authors()), self.authors())
 
-    
+
 
     def _authors_concat_string(self, author, authors):
         """
@@ -243,19 +244,16 @@ class View(BrowserView):
         if nr == 2:
             if authors[-1] == author:
                 return author['name']
-            #this is actually quite ugly. It relies on XSL-FO to produce the 
-            #necessary whitespace after 'and'. We had double-whitespace when 
-            #returning "and "    
-            return "%s and" %author['name'] 
+            #this is actually quite ugly. It relies on XSL-FO to produce the
+            #necessary whitespace after 'and'. We had double-whitespace when
+            #returning "and "
+            return "%s and" %author['name']
 
         if nr > 2:
             if authors[-1] == author:
                 return "and %s" %author['name']
             return "%s,"%author['name']
         return None
-
-
-
 
     def get_volume(self):
         """
@@ -266,22 +264,18 @@ class View(BrowserView):
         vol = cyear - startyear +1
         return vol
 
-     
     def special_issue(self):
         """
         checks if paper has been published in Special Issue
         """
-        obj = self.context
         paper_view = getMultiAdapter((self.context, self.request),
-                name="paperView")
+                                     name="paperView")
         si = paper_view.getSpecialIssues()
         if si:
-            obj = si[0].getObject()
-            title = escape(obj.Title())
-            url = obj.absolute_url()
-            return {'title' : title, 'url' : url}
+            brain = first(si)
+            return {'title': brain.Title, 'url': brain.getURL}
         return False
-    
+
 
     def escape_title(self):
         """
@@ -289,7 +283,7 @@ class View(BrowserView):
         title = self.context.Title()
         title = escape(title)
         return title
-        
+
 
     def escape_additional(self):
         ann = self.annotations()
